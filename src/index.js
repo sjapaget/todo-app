d0accfe7ece0e782068a7b34b394bf7a3d209bf3
Samuel Paget <sjapaget@gmail.com>
2022-11-24 11:31:36 +0100

    Fix formatting for merge

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import Displayer from './display.js';
 import Project from './project.js'
 import Task from './task.js';
 
-
 const root = document.getElementById('root');
   root.classList.add('main__container')
 
