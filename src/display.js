e8384fcb720925e4fc34dfddf94a0bf0e9735025
Samuel Paget <sjapaget@gmail.com>
2023-01-27 20:26:06 +0100

    Add cancel button to new project form

--- src/display.js
+++ src/display.js
@@ -211,6 +211,16 @@ export default class Displayer {
 
     form.appendChild(submitButton);
 
+    const cancelBtn = document.createElement('button');
+      cancelBtn.classList.add('form__cancel-btn');
+      cancelBtn.innerText = 'Cancel';
+      cancelBtn.onclick = (event) => {
+        event.preventDefault();
+        const formContainer = document.getElementsByClassName('form__container')[0];
+        formContainer.remove();
+      }
+    form.appendChild(cancelBtn);
+
     this.rootNode.appendChild(newProjectFormContainer);
   };
 
