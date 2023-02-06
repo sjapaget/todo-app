d2eb8dc9d68d4e81a014ff7ff58a36415d45fa79
Samuel Paget <sjapaget@gmail.com>
2023-02-06 23:23:43 +0100

    Remove commented out code

--- src/index.js
+++ src/index.js
@@ -2,22 +2,9 @@ import './sass/main.scss';
 import Displayer from './display';
 import Storer from './storage';
 import Project from './project';
-// import Task from './task';
 
 const root = document.getElementById('root');
 root.classList.add('main__container');
-
-// const exampleTask = new Task({
-//   title: 'Example Task',
-//   description: 'This is an example task',
-//   dueDate: new Date('26/12/2023'),
-// });
-
-// const exampleTaskTwo = new Task({
-//   title: 'Example Task Two',
-//   description: 'Two - This is an example task',
-//   dueDate: new Date('22/12/2022'),
-// });
 
 const savedProjects = Storer.retrieveAll();
 let allProjects;
